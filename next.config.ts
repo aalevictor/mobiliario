ef9cb8e3ea9f47a24944d9ed4f1a17ccf4deadbc
Victor Abreu <aalevictor@gmail.com>
2025-08-19 13:48:07 -0300

    remove standalone config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig = { 
-  output: 'standalone',
+const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
   turbopack: {
     rules: {
